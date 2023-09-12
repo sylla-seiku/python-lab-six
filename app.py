4304f90c3f04690f648974fd11969ae45a5b8ad4
sylla-seiku <seikusylla@gmail.com>
2023-09-12 11:35:26 -0400

    Learning about Modules Practice.

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 #Learning about Modules.
 import converters # this imports an entire module.
 from converters import kg_to_lbs # this imports a spicefic class from a module.
+from converters import find_max
 
 
 print(converters.kg_to_lbs(70)) #on this line we have to prefix this object call with a name of an object.
 
 print(kg_to_lbs(10)) # in this example we dont 
+
+numbers = [10, 3, 6, 2]
+maximum = find_max(numbers)
+#print(max(numbers))
+print(maximum)
--- converters.py
+++ converters.py
@@ -1,6 +1,25 @@
+#Learning about Modules Practice.
+numbers = [10, 3, 6, 2]
+max = numbers [0]
+for number in numbers:
+    if number > max:
+        max = number 
+print(max) 
+
+#I'm going to creat a function out of the code above.
+def find_max(numbers):
+    maximum = [0]
+    for number in numbers:
+        if number > maximum:
+            maximum = number
+    return maximum
+
+
+'''
 #Learning about Modules.
 def lbs_to_kg(weight):
     return weight * 0.45
 
 def kg_to_lbs(weight):
     return weight / 0.45
+'''
