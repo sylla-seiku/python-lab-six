a3cbaace32c11c8fc8d4905729a349bb7a40a29d
sylla-seiku <seikusylla@gmail.com>
2023-09-12 15:08:51 -0400

    Learning about Generating Random Values.

---
+++ python.py
@@ -0,0 +1,27 @@
+#learning about Generating Random Values.
+import random #this is a built in python built in module.
+
+for i in range(3):
+    print(random.random())
+
+
+for i in range(3):
+    print(random.randint(10, 20))
+
+
+#Practice
+members = ['Seiku', 'Beyage', 'Ebrahim', 'Ahamadou', 'Kadijatou', 'Musa']
+leader = random.choice(members)
+print(leader)
+
+
+#Practice creating a Dice Game.
+class Dice:
+    def roll(self):
+        first = random.randint(1, 6)
+        secound = random.randint(1, 6)
+        return first, secound
+    
+
+dice = Dice()
+print(dice.roll())
